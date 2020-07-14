49ec4aad3276eafe2cc2cd3321cf4439bda77ef5
Bhimesh Chauhan <bhimeshchauhan@esarcbot.local>
2020-07-14 01:17:50 -0400

    spaceship and the background

---
+++ game-v1.0/Assets/scripts/spaceShipController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spaceShipController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Move Forward - move front - Key: 'W'
+
+    // Pitch the ship - nose and tail up-down tilt - Key: 'Mouse'
+
+    // Roll the ship - wing tip tilt - Key: 'A & D'
+
+    // Yaw the ship - nose and tail side ways tilt - Key: 'Mouse'
+}
